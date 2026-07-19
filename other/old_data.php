d241aae07f528e36ca71feb0ed22ffe1c0d263b0
legendbgmiacc7-cmd <legendbgmiacc7@gmail.com>
2026-07-19 20:32:49 +0530

    Add files via upload

---
+++ other/old_data.php
@@ -0,0 +1,201 @@
+{
+  "servers": [
+    {
+      "serverID": "0",
+      "name": "МОСКВА",
+      "online": "496",
+      "mult": "x2",
+      "region": "Europe",
+      "color": "#fdd835",
+      "maxonline": "1000",
+      "ip": "80.242.59.112",
+      "port": "1521",
+      "onlinePer30Min": "1740",
+      "onlinePer1Day": "4487",
+      "onlinePer7Day": "0",
+      "onlinePerMouth": "10650",
+      "locked": "0",
+      "develop": "0",
+      "open_at": "0",
+      "only_langs": "[\"ru\", \"uk\", \"be\", \"kk\", \"uz\", \"ky\", \"tg\", \"tk\", \"az\", \"hy\", \"ro\"]",
+      "except_langs": null
+    },
+    {
+      "serverID": "1",
+      "name": "СЕВАСТОПОЛЬ",
+      "online": "424",
+      "mult": "x2",
+      "region": "Europe",
+      "color": "#47CCFF",
+      "maxonline": "1000",
+      "ip": "141.94.184.108",
+      "port": "1225",
+      "onlinePer30Min": "1894",
+      "onlinePer1Day": "1433",
+      "onlinePer7Day": "860",
+      "onlinePerMouth": "3426",
+      "locked": "0",
+      "develop": "0",
+      "open_at": "0",
+      "only_langs": "[\"ru\", \"uk\", \"be\", \"kk\", \"uz\", \"ky\", \"tg\", \"tk\", \"az\", \"hy\", \"ro\"]",
+      "except_langs": null
+    }
+  ],
+  "news": [
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history75.png",
+      "title": "Работа Дайвера",
+      "url": "https:\/\/t.me\/lr_dev\/744"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history74.png",
+      "title": "Весенний сезон!",
+      "url": "https:\/\/t.me\/lr_dev\/736"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history73.png",
+      "title": "Тюнинг центр!",
+      "url": "https:\/\/t.me\/lr_dev\/709"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history72.png",
+      "title": "Зимний сезон!",
+      "url": "https:\/\/t.me\/lr_dev\/669"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history70.png",
+      "title": "Авиакомпании!",
+      "url": "https:\/\/t.me\/lr_dev\/653"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history69.png",
+      "title": "Хэллоуин!",
+      "url": "https:\/\/t.me\/lr_dev\/640"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history68.png",
+      "title": "Рыбалка!",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_102331"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history66.png",
+      "title": "Новый сервер!",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_101450"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history64.png",
+      "title": "Платформы",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_87023"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history62.png",
+      "title": "150 уровней",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_86865"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history61.png",
+      "title": "Дерби",
+      "url": "https:\/\/youtu.be\/iwb7aGNne3k"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history59.png",
+      "title": "Питомцы\/Зоомагазин",
+      "url": "https:\/\/youtu.be\/i_skFg7uSdM"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history58.png",
+      "title": "Фермы\/Охота\/Лесоруб",
+      "url": "https:\/\/youtu.be\/G2rCpoobYiA"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history57.png",
+      "title": "Боевой пропуск!",
+      "url": "https:\/\/vk.com\/liverussia.mobile?w=wall-210807277_1709"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history56.png",
+      "title": "Нам 1 год!",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_56202"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history55.png",
+      "title": "Барахолка",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_55962"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history54.png",
+      "title": "Майнинг фермы",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_55627"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history53.png",
+      "title": "Работа МЧС",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_55547"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history52.png",
+      "title": "Данжи",
+      "url": "https:\/\/vk.com\/video777832425_456239307"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history51.png",
+      "title": "Игровой центр",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_54257"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history50.png",
+      "title": "Голосовой чат",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_45076"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history49.png",
+      "title": "Теплицы",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_31712"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history48.png",
+      "title": "Отдел кадров",
+      "url": "https:\/\/vk.com\/liverussia.media?w=wall-210807331_1454"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history47.png",
+      "title": "Реальные пацаны",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_30035"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history46.png",
+      "title": "Достижения",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_25316"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history45.png",
+      "title": "Сокровища",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_18773"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history44.png",
+      "title": "Работа механика",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_12448_r12459"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history43.png",
+      "title": "Стайлинг центр",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_2908_r2970"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history42.png",
+      "title": "Работа шахтёра",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_122"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history41.png",
+      "title": "Работа лесоруба",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_66"
+    },
+    {
+      "imageurl": "https:\/\/files.liverussia.online\/historyforlauncher\/history40.png",
+      "title": "Открытие сервера",
+      "url": "https:\/\/vk.com\/liverussia.online?w=wall-210807346_996"
+    }
+  ]
+}
